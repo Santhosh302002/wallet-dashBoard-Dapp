65171e5c63a548f0988977738e586c411a6ddca8
Santhosh302002 <l.santhoshkumar30@gmail.com>
2023-05-19 16:33:36 +0530

    frontend

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import Header from "../components/Header";
-import LotteryEntrance from "../components/LotteryEntrance";
+import Wallet from "../components/Wallet";
 import { useMoralis } from "react-moralis";
 
 const supportedChains = ["31337", "11155111"]
@@ -21,7 +21,7 @@ export default function Home() {
         <div>
           {supportedChains.includes(parseInt(chainId).toString()) ? (
             <div className="flex flex-row">
-              <LotteryEntrance className="p-8" />
+              <Wallet />
             </div>
           ) : (
             <div>{`Please switch to a supported chainId. The supported Chain Ids are: ${supportedChains}`}</div>
